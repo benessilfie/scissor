f158711912e7c29d410ad262112718979ef6d856
Essilfie <bquansah007@gmail.com>
2023-06-25 19:47:08 +0000

    Setup graphql and annotate gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'application#healthcheck'
+  post '/', to: 'graphql#execute'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  def healthcheck
+    render json: { message: 'OK' }, status: :ok
+  end
 end
